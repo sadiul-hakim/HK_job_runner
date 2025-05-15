95a68b6dc9751fe9e0afc3ada29f4fe1e421888d
Sadiul Hakim <sadiulhakim@gmail.com>
2025-05-15 12:12:48 +0600

    Jobs and triggers are now stored in database not RAM

--- src/main/java/xyz/sadiulhakim/config/AppConfig.java
+++ src/main/java/xyz/sadiulhakim/config/AppConfig.java
@@ -3,13 +3,21 @@ package xyz.sadiulhakim.config;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.impl.DirectSchedulerFactory;
+import org.quartz.impl.jdbcjobstore.InvalidConfigurationException;
+import org.quartz.impl.jdbcjobstore.JobStoreTX;
 import org.quartz.simpl.RAMJobStore;
+import org.quartz.utils.ConnectionProvider;
+import org.quartz.utils.DBConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @EnableAsync
 @Configuration
@@ -24,9 +32,28 @@ public class AppConfig {
     }
 
     @Bean
-    public Scheduler scheduler() throws SchedulerException {
+    public Scheduler scheduler(DataSource dataSource) throws SchedulerException, InvalidConfigurationException {
         QuartzVirtualThreadPool virtualThreadPool = new QuartzVirtualThreadPool();
-        RAMJobStore jobStore = new RAMJobStore();
+        DBConnectionManager dbConnectionManager = DBConnectionManager.getInstance();
+        dbConnectionManager.addConnectionProvider("myDS", new ConnectionProvider() {
+            @Override
+            public Connection getConnection() throws SQLException {
+                return dataSource.getConnection();
+            }
+
+            @Override
+            public void shutdown() {
+            }
+
+            @Override
+            public void initialize() {
+            }
+        });
+
+        JobStoreTX jobStore = new JobStoreTX();
+        jobStore.setDataSource("myDS");
+        jobStore.setDriverDelegateClass("org.quartz.impl.jdbcjobstore.PostgreSQLDelegate");
+        jobStore.setTablePrefix("QRTZ_");
 
         DirectSchedulerFactory.getInstance().createScheduler(
                 "VirtualScheduler",
